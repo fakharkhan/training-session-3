d38ee23f80ad0da1ac8fde494e269187053b2fdc
Fakhar  Khan <fakhar@softpyramid.com>
2019-01-12 15:16:50 +0500

    Class 4: Installing Nova

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -26,7 +26,7 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Gate::define('create-student', function ($user) {
-            return $user->id == 1;
+            return $user->id == 9;
         });
 
     }
